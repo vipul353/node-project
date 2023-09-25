194739b3dd6cb243c4e652fedebbd6cc9f41201f
vipul353 <thakurbipul353@gmail.com>
2023-09-25 20:35:45 +0530

    updated

--- src/Component/GenricComponents/AllAdes.js
+++ src/Component/GenricComponents/AllAdes.js
@@ -1,8 +1,8 @@
 import React from 'react'
-
+import '../HomePage/Home.css';
 const AllAdes = () => {
   return (
-    <div className="ads">
+    <div className="ads-mobile">
     <img style={{height:'100%',width:'100%',overflow:'hidden'}} src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXz2SENaXMYUf3Ru2X76ufRjx3G9BI_Bj5sA&usqp=CAU'  alt="no data"/>
   </div>
   )
--- src/Component/HomePage/HomePage.js
+++ src/Component/HomePage/HomePage.js
@@ -195,10 +195,21 @@ const HomePage = () => {
                       return <></>;
                     }
                   })}
-
+                   
                   {/* <HorizontalSmall />
                 <HorizontalSmall />
                 <HorizontalSmall /> */}
+                 <div className="ads-mobile">
+                    <img
+                      style={{
+                        height: "100%",
+                        width: "100%",
+                        overflow: "hidden",
+                      }}
+                      src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXz2SENaXMYUf3Ru2X76ufRjx3G9BI_Bj5sA&usqp=CAU"
+                      alt="no data"
+                    />
+                  </div>
                 </div>
               </div>
             </div>
